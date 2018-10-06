9a5130defdde2e3ff82a67b4e26aee00a8a9f347
GonnenDaube <gonen.daube@gmail.com>
2018-10-07 00:41:35 +0300

    LayerConstructor-POC

--- WorldBuilder/WorldBuilder/wwwroot/js/site.js
+++ WorldBuilder/WorldBuilder/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    let pX = [0, 0, 20, 40, 49, 66, 86, 100, 100];
+    let pY = [100, 37, 20, 31, 19, 23, 18, 35, 100];
+    let polygon = convertToPolygon(pX, pY);
+    $('#layer0').attr('style', $('#layer0').attr('style') + 'clip-path: ' + polygon);
+});
+
+function convertToPolygon(pX, pY) {
+    let p = 'polygon(';
+    for (let i = 0; i < pX.length - 1; i++) {
+        p += pX[i] + '% ' + pY[i] + '%, ';
+    }
+    p += pX[pX.length - 1] + '% ' + pY[pY.length - 1] + '%);';
+    return p;
+}
