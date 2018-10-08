8598a85bb7f8f14707718da52f22efb324ae1a21
GonnenDaube <gonen.daube@gmail.com>
2018-10-08 21:11:31 +0300

    Curve POC finished

--- WorldBuilder/WorldBuilder/wwwroot/js/site.js
+++ WorldBuilder/WorldBuilder/wwwroot/js/site.js
@@ -4,9 +4,17 @@ var handleIndex = -1;
 let pointsX = [0, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 100];
 let pointsY = [100, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 100];
 
+let curveSmoothness = 20;
+
+let finalPX;
+let finalPY;
+
 $(document).ready(function () {
     positionHandles(pointsX, pointsY);
-    let polygon = convertToPolygon(pointsX, pointsY);
+    let curved = cruveLines(pointsX, pointsY);
+    finalPX = curved.x;
+    finalPY = curved.y;
+    let polygon = convertToPolygon(finalPX, finalPY);
     $('#layer0').attr('style', 'background-color:dodgerblue; clip-path: ' + polygon);
 
     $(document).on('mousedown', '.handle', function (e) {
@@ -44,10 +52,13 @@ function updatePointsArray() {
     let x, y;
     for (let i = 1; i < pointsX.length - 1; i++) {
         [x, y] = analyseStyle($('.handle:eq(' + (i - 1) + ')').attr('style'));
-        pointsX[i] = x;
-        pointsY[i] = y;
+        pointsX[i] = Number(x);
+        pointsY[i] = Number(y);
     }
-    let polygon = convertToPolygon(pointsX, pointsY);
+    let curved = cruveLines(pointsX, pointsY);
+    finalPX = curved.x;
+    finalPY = curved.y;
+    let polygon = convertToPolygon(finalPX, finalPY);
     $('#layer0').attr('style', 'background-color:dodgerblue; clip-path: ' + polygon);
 }
 
@@ -68,6 +79,43 @@ function convertToPolygon(pX, pY) {
     }
     p += pX[pX.length - 1] + '% ' + pY[pY.length - 1] + '%);';
     return p;
+}
+
+function cruveLines(pX, pY) {
+    let x = [];
+    x.push(pX[0]);
+    x.push(pX[1]);
+    let y = [];
+    y.push(pY[0]);
+    y.push(pY[1]);
+    let segment;
+    
+    for (let i = 1; i < pX.length - 3; i += 2) {
+        segment = getCurveSegment(pX[i], pX[i + 1], pX[i + 2], pY[i], pY[i + 1], pY[i + 2]);
+        x = x.concat(segment.x);
+        y = y.concat(segment.y);
+    }
+
+    x.push(pX[pX.length - 1]);
+    y.push(pY[pY.length - 1]);
+    return { 'x': x, 'y': y };
+}
+
+function getCurveSegment(x1, x2, x3, y1, y2, y3) {
+    //y = ax^2 + bx + c
+    let a = ((y1 - y2) * (x2 - x3) - (y2 - y3) * (x1 - x2)) / ((x1 * x1 - x2 * x2) * (x2 - x3) - (x2 * x2 - x3 * x3) * (x1 - x2));
+    let b = ((y1 - y2) * (x2 - x3) - a * (x1 * x1 - x2 * x2) * (x2 - x3)) / ((x1 - x2) * (x2 - x3));
+    let c = y1 - a * x1 * x1 - b * x1;
+
+    let diff = x3 - x1;
+    let step = diff / curveSmoothness;
+    let x = [];
+    let y = [];
+    for (let i = 1; i <= curveSmoothness; i++) {
+        x.push(x1 + i * step);
+        y.push(a * x[i - 1] * x[i - 1] + b * x[i - 1] + c);
+    }
+    return { 'x': x, 'y': y };
 }
 
 function positionHandles(pX, pY) {
